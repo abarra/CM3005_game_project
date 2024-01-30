b0b7d638ea9db24c212eed789fa7eab3a55f9bf8
Alexis Barra <alexis.barra@gmail.com>
2024-01-29 17:40:56 -0800

    Adding basic game manager and project structure

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
+    
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    
+    private void Start()
+    {
+        // Initialize the game
+        InitializeGame();
+    }
+    
+    private void InitializeGame()
+    {
+        // Load the first level
+        LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    private void LoadLevel(int levelIndex)
+    {
+        // Load the specified level
+        SceneManager.LoadScene(levelIndex);
+    }
+}
