30eca7048488bd0c83f4a9e9289f2030a8d26224
Alexis Barra <alexis@nightgardenstudio.com>
2024-02-02 16:23:42 -0800

    Adding base view class

---
+++ Assets/Scripts/Views/View.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        
+    }
+
+    public virtual void Active()
+    {
+
+    }
+
+    public virtual void DeActive()
+    {
+        
+    }
+
+    public virtual void CloseView()
+    {
+        UIManager.Instance.DeActiveView(this.gameObject.name);
+    }
+
+}
