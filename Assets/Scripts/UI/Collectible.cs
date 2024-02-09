ca1e5beb467a124839dfa7ee64a20ebbb68cee40
Alexis Barra <alexis.barra@gmail.com>
2024-02-09 10:49:12 -0800

    Importing package from Hristo

---
+++ Assets/Scripts/UI/Collectible.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public SphereCollider col;
+    public bool isCollected = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //you can change if to any collider type or get other.gameObject and compare tag, or name, or layer, or whatever you need
+        if (other.GetType().ToString() == "BoxCollider")
+        {
+            
+        }
+        Debug.Log(other.GetType().ToString());
+        isCollected = true;
+    }
+}
