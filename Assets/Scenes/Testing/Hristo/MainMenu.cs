50e9cc89dceb5bef2ae7913924bfa5475ab8df33
Hristo Stantchev <hristostantchev@abv.bg>
2024-02-11 17:20:38 +0200

    (on Hristo scene) Main Menu in Views in; Temp scripts in Testing/Hristo folder

---
+++ Assets/Scenes/Testing/Hristo/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class MainMenu : View
+{
+    [SerializeField] List<GameObject> menuList;
+    /**
+     Loads scene given sceneName string parameter.
+     Use on Button OnClick method in Editor.
+     Add the name of the scene i.e. "Garage" as parameter here.
+     **/
+    public void OnButtonPressLoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
+    /***
+    Enables selected menu given as string parameter, disables all else.
+    Use on Button OnClick Function in Editor.
+    Add the name of the menu i.e. "Level Select" as parameter.
+    ***/
+    public void OnButtonPressLoadMenu(string menuName)
+    {
+        //set inactive for all other sub-menus
+        menuList.Where(n => n.name != menuName).ToList().ForEach(n => n.SetActive(false));
+        //set active for selected sub-menu
+        GameObject activeMenu = menuList.Find(x => x.name == menuName);
+        activeMenu.SetActive(true);
+    }
+}
---
+++ Assets/Scenes/Testing/Hristo/TempFooterOverlay.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempFooterOverlay : MonoBehaviour
+{
+    public void OpenMainMenu()
+    {
+        UIManager.Instance.ActivateView("MainMenu");
+    }
+}
