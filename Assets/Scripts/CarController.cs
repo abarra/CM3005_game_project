ae57762ba9740360e95f4c2e13a69d34eb53e9f6
Mihail Krylov <mihail@johnhelp.ru>
2024-02-23 11:27:34 +0700

    Move driving and camera script and attemt to apply them to the car

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private const float SlipAngleMax = 120f;
+    private const float FrontBrakeMultiplier = 0.7f;
+    private const float RearBrakeMultiplier = 0.3f;
+
+    // Debug display vars
+    public WheelColliders colliders;
+    public WheelMeshes wheelMeshes;
+
+    public float accelerationSignal;
+    public float steeringInput;
+    public float brakeSignal;
+
+    public float motorTorque;
+    public float brakeTorque;
+    public AnimationCurve steeringCurve;
+    
+    public float speed;
+    private float slipAngle;
+    private Rigidbody rb;
+
+    private IInputController inputController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        inputController = FindObjectOfType<InputController>();
+        inputController.SubscribeInputEvents(OperateGasAndSteering);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = rb.velocity.magnitude;
+        ComputeAcceleration();
+        ComputeBrake();
+        ComputeSteering();
+        UpdateWheelsPosition();
+    }
+
+    private void OperateGasAndSteering(InputData data)
+    {
+      
+        accelerationSignal = data.Vertical;
+        steeringInput = data.Horizontal;
+
+        // Calculate slip angle
+        slipAngle = Vector3.Angle(transform.forward, rb.velocity - transform.forward);
+        if (slipAngle < SlipAngleMax && accelerationSignal < 1f)
+        {
+            if (accelerationSignal < 0f)
+            {
+                brakeSignal = Mathf.Abs(accelerationSignal);
+                accelerationSignal = 0f;
+            }
+        }
+        else
+        {
+            brakeSignal = 0f;
+        }
+    }
+
+    private void ComputeBrake()
+    {
+        colliders.FLWheel.brakeTorque = brakeSignal * brakeTorque * FrontBrakeMultiplier;
+        colliders.FRWheel.brakeTorque = brakeSignal * brakeTorque * FrontBrakeMultiplier;
+        colliders.RLWheel.brakeTorque = brakeSignal * brakeTorque * RearBrakeMultiplier;
+        colliders.RRWheel.brakeTorque = brakeSignal * brakeTorque * RearBrakeMultiplier;
+    }
+
+    private void ComputeAcceleration()
+    {
+        colliders.RLWheel.motorTorque = motorTorque * accelerationSignal;
+        colliders.RRWheel.motorTorque = motorTorque * accelerationSignal;
+    }
+
+    private void ComputeSteering()
+    {
+        // Calculate steering angle
+        var steeringAngle = steeringInput * steeringCurve.Evaluate(speed);
+        colliders.FLWheel.steerAngle = steeringAngle;
+        colliders.FRWheel.steerAngle = steeringAngle;
+    }
+
+    private void UpdateWheelsPosition()
+    {
+        UpdateWheel(colliders.FLWheel, wheelMeshes.FLWheel);
+        UpdateWheel(colliders.FRWheel, wheelMeshes.FRWheel);
+        UpdateWheel(colliders.RLWheel, wheelMeshes.RLWheel);
+        UpdateWheel(colliders.RRWheel, wheelMeshes.RRWheel);
+    }
+
+    private static void UpdateWheel(WheelCollider coll, Transform wheelMesh)
+    {
+        coll.GetWorldPose(out var position, out var quat);
+        Debug.LogWarning(position.x);
+        wheelMesh.position = position+new Vector3(10,0,0);
+        wheelMesh.rotation = quat;
+    }
+}
---
+++ Assets/Scripts/WheelMeshes.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class WheelMeshes
+{
+    public Transform FLWheel;
+    public Transform FRWheel;
+    public Transform RLWheel;
+    public Transform RRWheel;
+
+}
