ba6ffa292af3e1375e8e0692ad125ed25992a8cd
Marco Rizzo <marcrizz@gmail.com>
2024-02-24 16:04:58 -0800

    Create a simple Minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+
+    void LateUpdate()
+    {
+        var newPos = player.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
