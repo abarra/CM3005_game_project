43d118a6998e77058d7b0d226225833a38aefb45
Ilya Amelevich <ilya.amelevich@gmail.com>
2024-02-26 00:26:40 +0100

    feat: game over view

---
+++ Assets/Scripts/Views/GameOverView.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverView : MenuView
+{
+    public TextMeshProUGUI scoreText;
+
+    protected void OnEnable()
+    {
+        scoreText.text = ScoreManager.Instance.FormattedScore;
+    }
+
+    /// <summary>
+    /// Close pause menu and open MainMenuView
+    /// </summary>
+    public void ToMainMenu()
+    {
+        UIManager.Instance.ActivateView("MainMenuView");
+        CloseView();
+    }
+}
