494aad7b8fd47194be146fbafabc0521530b65bb
Ilya Amelevich <ilya.amelevich@gmail.com>
2024-03-01 21:36:00 +0100

    feat: add speedometer controller

---
+++ Assets/Scripts/SpeedometerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+public class SpeedometerController : MonoBehaviour
+{
+    public TextMeshProUGUI speedText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.State == GameManager.GameState.running)
+        {
+            CarController car = (CarController)FindObjectOfType(typeof(CarController));
+            if (car)
+            {
+                var nfi = new CultureInfo( "en-US", false ).NumberFormat;
+                nfi.NumberDecimalDigits = 0;
+                speedText.text = car.speed.ToString("N", nfi);
+            }
+            else
+            {
+                Debug.Log("Car not found");
+            }
+        }
+    }
+}
