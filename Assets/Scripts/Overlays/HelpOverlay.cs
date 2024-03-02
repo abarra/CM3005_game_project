615ee30b672775387439aeb2dbd6d89548c7b457
Ilya Amelevich <ilya.amelevich@gmail.com>
2024-03-02 17:43:36 +0100

    feat: help window that can be hide/unhide

---
+++ Assets/Scripts/Overlays/HelpOverlay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpOverlay : MonoBehaviour
+{
+    public GameObject helpWindow;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            ToggleVisibility();
+        }
+    }
+
+    public void ToggleVisibility()
+    {
+        if (helpWindow.activeSelf)
+        {
+            Hide();
+        }
+        else
+        {
+            Show();
+        }
+    }
+
+    public void Show()
+    {
+        Debug.Log("[HelpOverlay] Showing");
+        helpWindow.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        Debug.Log("[HelpOverlay] Hiding");
+        helpWindow.SetActive(false);
+    }
+}
