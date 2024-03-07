afd4fe5fc24a4400d155d1230bd9718710ede3db
Hristo Stantchev <hristostantchev@abv.bg>
2024-03-07 18:25:53 +0200

    Finish line and score collectible placements in + collectibles larger

--- Assets/Barrier Pack/Scripts/VictoryZone.cs
+++ Assets/Barrier Pack/Scripts/VictoryZone.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class VictoryZone : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Material material;
+    [SerializeField, Range(0f, 1f)] float minAlpha;
+    [SerializeField, Range(0f, 1f)] float maxAlpha;
+
+    float clrChangeAmount = 0.01f;
+    bool reachedMin = false;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeMaterialAlpha();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player Triggered");
+            GameManager.Instance.Win();
+        }
+    }
+    void ChangeMaterialAlpha()
+    {
+        if (material != null)
+        {
+            Color newClr = material.color;
+            if (reachedMin)
+            {
+                newClr.a += Mathf.Clamp01(clrChangeAmount);
+                if (newClr.a >= maxAlpha)
+                {
+                    newClr.a = maxAlpha;
+                    reachedMin = false;
+                }
+            }
+            else
+            {
+                newClr.a -= Mathf.Clamp01(clrChangeAmount);
+                if (newClr.a <= minAlpha)
+                {
+                    newClr.a = minAlpha;
+                    reachedMin = true;
+                }
+            }
+            material.color = newClr;
+        }
     }
 }
