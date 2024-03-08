6f942e1a93980abbfc52a174b723809dfb3a6bc7
Alexis Barra <alexis.barra@gmail.com>
2024-03-08 10:49:57 -0800

    Fixing compilation errors

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static Enums;
-using static UnityEditorInternal.VersionControl.ListControl;
+
 public class CarController : MonoBehaviour
 {
     public CarStates state = CarStates.neutral;
